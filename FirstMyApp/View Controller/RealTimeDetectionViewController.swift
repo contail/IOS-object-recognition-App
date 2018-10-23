700756da4c85042160f81749173330fa7d71637c
contail <qkrtkdwls2121@gmail.com>
2018-10-23 19:01:37 +0900

    add RealtimeCamere View

--- FirstMyApp/View Controller/RealTimeDetectionViewController.swift
+++ FirstMyApp/View Controller/RealTimeDetectionViewController.swift
@@ -2,7 +2,7 @@
 //  RealTimeDetectionViewController.swift
 //  FirstMyApp
 //
-//  Created by sangjin park on 2018. 9. 5..
+//  Created by sangjin park on 2018. 10. 23..
 //  Copyright © 2018년 Loguin. All rights reserved.
 //
 
@@ -11,33 +11,32 @@ import UIKit
 class RealTimeDetectionViewController: UIViewController {
 
     @IBOutlet weak var cameraView: UIView!
-    var videoCapture: VideoCapture!
+    var videoCapture : VideoCapture!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let spec = VideoSpec(fps: 3, size: CGSize(width: 1280, height: 720))
+        
+        let spec = VideoSpec(fps :3, size : CGSize(width: 1280, height: 720))
         self.videoCapture = VideoCapture(cameraType: .back, preferredSpec: spec, previewContainer: self.cameraView.layer)
-        self.videoCapture.imageBufferHandler = { (imageBuffer, timestamp, outputBuffer) in
-            
+        
+        self.videoCapture.imageBufferHandler = {(imageBuffer, timestamp, outputBuffer) in
+        
         }
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         if let videoCapture = self.videoCapture {
             videoCapture.startCapture()
         }
         
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        if let videoCapture = self.videoCapture{
+        if let videoCapture = self.videoCapture {
             videoCapture.stopCapture()
         }
     }
-   
-
 }
