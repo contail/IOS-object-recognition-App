d86c25a3a794572b7c48d2668deec1c40ef73a50
contail <qkrtkdwls2121@gmail.com>
2018-10-23 21:11:11 +0900

    add realtime recoginze

--- FirstMyApp/View Controller/RealTimeDetectionViewController.swift
+++ FirstMyApp/View Controller/RealTimeDetectionViewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
-
+import Vision
 class RealTimeDetectionViewController: UIViewController {
 
     @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var categoryView: UILabel!
+    @IBOutlet weak var confidenceView: UILabel!
     var videoCapture : VideoCapture!
-    
+    let visonRequestHanlder = VNSequenceRequestHandler()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.categoryView.text = ""
+        self.confidenceView.text = ""
         
         let spec = VideoSpec(fps :3, size : CGSize(width: 1280, height: 720))
         self.videoCapture = VideoCapture(cameraType: .back, preferredSpec: spec, previewContainer: self.cameraView.layer)
         
         self.videoCapture.imageBufferHandler = {(imageBuffer, timestamp, outputBuffer) in
-        
+        self.detectObject(image: imageBuffer)
         }
     }
     
@@ -37,6 +42,27 @@ class RealTimeDetectionViewController: UIViewController {
         
         if let videoCapture = self.videoCapture {
             videoCapture.stopCapture()
+        }
+    }
+    
+    func detectObject(image: CVImageBuffer){
+        
+            do{
+                let vnCoreMLModel = try VNCoreMLModel(for: Inceptionv3().model)
+                let request = VNCoreMLRequest(model: vnCoreMLModel, completionHandler: handleObjectDection)
+                request.imageCropAndScaleOption = .centerCrop
+                try self.visonRequestHanlder.perform([request], on: image)
+            }
+            catch{
+                print(error)
+            }
+            
+        
+    }
+    
+    func handleObjectDection(request: VNRequest, error: Error?) {
+        if let result = request.results?.first as? VNClassificationObservation {
+            print("\(result.identifier) :  \(result.confidence)")
         }
     }
 }
